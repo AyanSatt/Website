38d1ad93d27e94ad2d57eda94f86a8822e243e69
AyanSatt <72308147+AyanSatt@users.noreply.github.com>
2024-10-02 14:34:06 +0000

    Added Dynamic Content and route api

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app=Flask(__name__)
+JOBS=[
+  {
+    'id':1,
+    'title':'Data analyst',
+    'location':'kolkata',
+    'salary':'rs 1000000'
+  },
+  {
+    'id':2,
+    'title':'Data Engineer',
+    'location':'kolkata',
+    'salary':'rs 1200000'
+  },
+  {
+    'id':3,
+    'title':'AI ngineer',
+    'location':'kolkata',
+    'salary':'rs 1500000'
+  },
+  {
+    'id':4,
+    'title':'Backend developer',
+    'location':'kolkata'
+  }
+]
 @app.route("/")
 def hello():
-  return render_template("home.html")
+  return render_template("home.html",jobs=JOBS,company_name='xyz')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
